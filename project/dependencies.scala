9dd0160d3bfc39760f4cefde7cac3ce657aea43e
liweichao <weichao092@163.com>
2014-04-18 15:22:07 +0800

    change project settings

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -2,17 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  val typesafeReleases = "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
-  val typesafeSnapshots = "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
-  val local = "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
-  val akkap_tck = "krasserm at bintray" at "http://dl.bintray.com/krasserm/maven"
+    val coinport_repo = "coinport-repo" at "http://192.168.0.105:8081/nexus/content/groups/public"
+    val typesafeReleases = "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+    val typesafeSnapshots = "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+    val local = "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
+    val akkap_tck = "krasserm at bintray" at "http://dl.bintray.com/krasserm/maven"
 
-  val projectResolvers = Seq(
-    typesafeReleases,
-    typesafeSnapshots,
-    Resolver.sonatypeRepo("snapshots"),
-    akkap_tck
-  )
+    val projectResolvers = Seq(
+        coinport_repo,
+        typesafeReleases,
+        typesafeSnapshots,
+        Resolver.sonatypeRepo("snapshots"),
+        akkap_tck
+        
+        )
 
   val scalatest = "org.scalatest" %% "scalatest" % "2.0" % "test"
   val junit = "junit" % "junit" % "4.11" % "test"
@@ -21,11 +24,12 @@ object Dependencies {
   val embedMongoScalatest = "com.github.simplyscala" %% "scalatest-embedmongo" % "0.2.1" % "test"
   val embedMongo = "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "1.42" % "test"
   val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.3.0" % "test"
-  val akkaPersistTck = "com.github.krasserm" %% "akka-persistence-testkit" % "0.2" % "test"
+  val akkaPersistTck = "com.github.krasserm" %% "akka-persistence-testkit" % "0.3" % "test"
 
   val akkaPersistence = "com.typesafe.akka" %% "akka-persistence-experimental" % "2.3.0"
   val rxMongo = "org.reactivemongo" %% "reactivemongo" % "0.10.0"
   val casbah = "org.mongodb" %% "casbah" % "2.6.4"
+  val metrix = "nl.grons" %% "metrics-scala" % "3.0.5_a2.3"
  
   val testDependencies = Seq(
     scalatest,
@@ -36,7 +40,8 @@ object Dependencies {
   )
  
   val commonDependencies = Seq(
-    akkaPersistence
+    akkaPersistence,
+    metrix
   ) ++ testDependencies
 
   val casbahDependencies = Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 scalaVersion := "2.10.3"
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.0")
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0") 
